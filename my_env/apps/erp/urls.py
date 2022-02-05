5b2aca7a95722664f58ccd270ef8e2182e26a44c
jhandrylapo06 <jhandry.lapo@unl.edu.ec>
2022-02-05 00:08:39 -0500

    Statics y bootstrap

--- my_env/apps/erp/urls.py
+++ my_env/apps/erp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from erp.views import primeravista,segundavista
+
+app_name = 'erp'
 urlpatterns = [
     
-    path('vista1/',primeravista),
-    path('vista2/',segundavista),
+    path('vista1/',primeravista, name='vista1' ),
+    path('vista2/',segundavista, name='vista2' ),
 ]
